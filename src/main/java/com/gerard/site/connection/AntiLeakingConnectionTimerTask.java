c0fc911eed7d4e862334b9c17b3fae8577cf8c3b
lilu <sidelnikova.liliya@gmail.com>
2021-08-11 21:18:00 +0300

    OutputSiteDescription class renamed to OutputSiteDescriptionTag

--- src/main/java/com/gerard/site/connection/AntiLeakingConnectionTimerTask.java
+++ src/main/java/com/gerard/site/connection/AntiLeakingConnectionTimerTask.java
@@ -3,8 +3,10 @@ package com.gerard.site.connection;
 import java.util.TimerTask;
 
 public class AntiLeakingConnectionTimerTask extends TimerTask {
+
     @Override
     public void run() {
         ConnectionPool.getInstance().offerLeakedConnections();
     }
+
 }
